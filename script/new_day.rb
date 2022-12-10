0c6471f018b404243d69da43040beaa14e538479
Tom Harris <thomas.harris@smartbear.com>
2022-12-10 22:08:32 +0000

    add empty input file to new_day script

--- script/new_day.rb
+++ script/new_day.rb
@@ -4,6 +4,7 @@ day = ARGV[0]
 
 solution_file = "solutions/day_#{day}.rb"
 spec_file = "spec/day_#{day}_spec.rb"
+input_file = "input/input_#{day}.txt"
 
 # Outline solution file. Don't overwrite it if it already exists.
 if !File.exists?(solution_file)
@@ -14,3 +15,6 @@ end
 # Create spec file.
 spec_file_contents = File.read('script/templates/spec_template').gsub("<<day>>", day)
 File.write(spec_file, spec_file_contents)
+
+# Empty input file
+File.write(input_file, "")
