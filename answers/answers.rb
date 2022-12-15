a0b032e8482d9916147467fef8fdb3d3b719587f
Tom Harris <thomas.harris@smartbear.com>
2022-12-15 22:52:12 +0000

    add gitignore

--- answers/answers.rb
+++ answers/answers.rb
@@ -1,1 +1,103 @@
-# REDACTED
+require_relative 'constants'
+ANSWERS = {
+  day_1: { 
+    part_1: 69912,
+    part_2: 208180
+  },
+  day_2: { 
+    part_1: 10404,
+    part_2: 10334
+  },
+  day_3: { 
+    part_1: 8252,
+    part_2: 2828
+  }, 
+  day_4: {
+    part_1: 513,
+    part_2: 878
+  },
+  day_5: {
+    part_1: 'RNZLFZSJH',
+    part_2: 'CNSFCGJSM'
+  },
+  day_6: {
+    part_1: 1544,
+    part_2: 2145
+  },
+  day_7: {
+    part_1: 1908462,
+    part_2: 3979145
+  },
+  day_8: {
+    part_1: 1676,
+    part_2: 313200
+  },
+  day_9: {
+    part_1: 5858,
+    part_2: 2602
+  },
+  day_10: {
+    part_1: 17940,
+    part_2: DAY_10_PART_2
+  },
+  day_11: {
+    part_1: 118674,
+    part_2: 32333418600
+  },
+  day_12: {
+    part_1: 383,
+    part_2: 377
+  },
+  day_13: {
+    part_1: 6428,
+    part_2: 22464
+  },
+  day_14: {
+    part_1: 979,
+    part_2: 29044
+  },
+  day_15: {
+    part_1: 6078701,
+    part_2: 12567351400528
+  },
+  day_16: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_17: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_18: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_19: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_20: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_21: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_22: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_23: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_24: {
+    part_1: nil,
+    part_2: nil
+  },
+  day_25: {
+    part_1: nil,
+    part_2: nil
+  }
+}
